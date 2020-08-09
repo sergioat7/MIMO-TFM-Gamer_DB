f895f72ea9b03d76d0ae3f89783494db73c4155d
Sergio Aragonés <sergioat7@gmail.com>
2020-08-10 01:14:18 +0200

    Code refactoring

--- GamerDB/app/src/main/java/es/upsa/mimo/gamerdb/network/apiclient/APIClient.java
+++ GamerDB/app/src/main/java/es/upsa/mimo/gamerdb/network/apiclient/APIClient.java
@@ -4,6 +4,9 @@ import com.google.gson.*;
 import java.lang.reflect.Type;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
+
+import es.upsa.mimo.gamerdb.R;
+import es.upsa.mimo.gamerdb.models.ErrorResponse;
 import es.upsa.mimo.gamerdb.utils.Constants;
 import okhttp3.OkHttpClient;
 import retrofit2.Call;
@@ -15,14 +18,8 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class APIClient {
 
     private static Gson getGson() {
-
         return new GsonBuilder()
-                .registerTypeAdapter(Date.class, new JsonDeserializer<Date>() {
-                    @Override
-                    public Date deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
-                        return Constants.stringToDate(json.toString());
-                    }
-                })
+                .registerTypeAdapter(Date.class, (JsonDeserializer<Date>) (json, typeOfT, context) -> Constants.stringToDate(json.toString()))
                 .setDateFormat(Constants.dateFormat)
                 .serializeNulls()
                 .create();
@@ -44,7 +41,7 @@ public class APIClient {
                 .build();
     }
 
-    public static <T, U> void sendServer(Call<T> request, final CompletionHandler<T, U> completion) {
+    public static <T> void sendServer(Call<T> request, final CompletionHandler<T> completion) {
 
         request.enqueue(new Callback<T>() {
             @Override
@@ -53,13 +50,20 @@ public class APIClient {
                 if (response.isSuccessful() && response.body() != null) {
                     completion.success(response.body());
                 } else {
-                    completion.failure(null);//TODO
+                    completion.failure(
+                            new ErrorResponse("",
+                                    R.string.error_server,
+                                    "Error in APIClient onResponse block"));
                 }
             }
 
             @Override
             public void onFailure(Call<T> call, Throwable t) {
-                completion.failure(null);//TODO
+                completion.failure(
+                        new ErrorResponse("",
+                                R.string.error_server,
+                                "Error in APIClient onFailure block")
+                );
             }
         });
     }
