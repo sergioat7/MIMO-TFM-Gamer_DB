3676433280b752a84fa692673be06b1369bbc2f7
Sergio Aragonés <sergioat7@gmail.com>
2020-08-11 00:40:46 +0200

    Created game list view

--- GamerDB/app/src/main/java/es/upsa/mimo/gamerdb/activities/MainActivity.java
+++ GamerDB/app/src/main/java/es/upsa/mimo/gamerdb/activities/MainActivity.java
@@ -1,52 +1,27 @@
 package es.upsa.mimo.gamerdb.activities;
 
-import android.os.Bundle;
-import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+import android.os.Bundle;
+import butterknife.BindView;
 import butterknife.ButterKnife;
-import butterknife.OnClick;
 import es.upsa.mimo.gamerdb.R;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.swipe_refresh_layout)
+    SwipeRefreshLayout swipeRefreshLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
         ButterKnife.bind(this);
-    }
 
-    @OnClick(R.id.fab)
-    public void onFabClick(View view) {
-        Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show();
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+        swipeRefreshLayout.setColorSchemeResources(R.color.colorPrimary);
+        swipeRefreshLayout.setProgressBackgroundColorSchemeResource(android.R.color.white);
+        swipeRefreshLayout.setOnRefreshListener(() -> {
+            //TODO
+        });
     }
 }
