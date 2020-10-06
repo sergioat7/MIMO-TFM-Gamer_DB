915b530e4f89a8ed714c1ce933a3f3d1b4db2ba3
Sergio Aragonés <sergioat7@gmail.com>
2020-10-06 17:33:39 +0200

    Added tests to game detail view

--- GamerDB/app/src/androidTest/java/es/upsa/mimo/gamerdb/activities/GameDetailActivityTest.java
+++ GamerDB/app/src/androidTest/java/es/upsa/mimo/gamerdb/activities/GameDetailActivityTest.java
@@ -7,6 +7,7 @@ package es.upsa.mimo.gamerdb.activities;
 
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewParent;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.test.espresso.UiController;
 import androidx.test.espresso.ViewAction;
@@ -32,6 +33,7 @@ import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.isRoot;
 import static androidx.test.espresso.matcher.ViewMatchers.withClassName;
+import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.allOf;
@@ -56,14 +58,13 @@ public class GameDetailActivityTest {
 
     @Before
     public void setUp() {
-
         onView(isRoot()).perform(waitFor(TIME_TO_WAIT));
     }
 
     @Test
         public void testCorrectMainGameData() {
 
-        onView(withId(R.id.recycler_view_games)).perform(RecyclerViewActions.scrollToPosition(0), RecyclerViewActions.actionOnItemAtPosition(0, click()));
+        goToDetail();
 
         onView(withId(R.id.button_watch_video)).perform(scrollTo()).check(matches(isDisplayed()));
         onView(withId(R.id.button_view_images)).perform(scrollTo()).check(matches(isDisplayed()));
@@ -74,14 +75,17 @@ public class GameDetailActivityTest {
         onView(withId(R.id.text_view_game_series_title)).perform(scrollTo()).check(matches(isDisplayed()));
         onView(withId(R.id.recycler_view_game_series)).perform(scrollTo()).check(matches(isDisplayed()));
         onView((withId(R.id.recycler_view_game_series))).check(matches(hasNumberOfChildren(-1)));
+        onView(childAtPosition(childAtPosition(withId(R.id.recycler_view_game_series), 0), 2)).check(matches(not(isDisplayed())));
 
         onView(withId(R.id.text_view_games_suggested_title)).perform(scrollTo()).check(matches(isDisplayed()));
         onView(withId(R.id.recycler_view_games_suggested)).perform(scrollTo()).check(matches(isDisplayed()));
         onView((withId(R.id.recycler_view_games_suggested))).check(matches(hasNumberOfChildren(GAMES_COUNT + 1)));
+        onView(childAtPosition(childAtPosition(withId(R.id.recycler_view_games_suggested), 0), 2)).check(matches(not(isDisplayed())));
 
         onView(withId(R.id.text_view_game_additions_title)).perform(scrollTo()).check(matches(isDisplayed()));
         onView(withId(R.id.recycler_view_game_additions)).perform(scrollTo()).check(matches(isDisplayed()));
         onView((withId(R.id.recycler_view_game_additions))).check(matches(hasNumberOfChildren(-1)));
+        onView(childAtPosition(childAtPosition(withId(R.id.recycler_view_game_additions), 0), 2)).check(matches(not(isDisplayed())));
 
         onView(withId(R.id.text_view_developers_title)).perform(scrollTo()).check(matches(isDisplayed()));
         onView(withId(R.id.recycler_view_developers)).perform(scrollTo()).check(matches(isDisplayed()));
@@ -95,7 +99,7 @@ public class GameDetailActivityTest {
     @Test
     public void testShowCompleteDescription() {
 
-        onView(withId(R.id.recycler_view_games)).perform(RecyclerViewActions.scrollToPosition(0), RecyclerViewActions.actionOnItemAtPosition(0, click()));
+        goToDetail();
 
         onView(allOf(withId(R.id.button_show_more_text), isDisplayed())).perform(scrollTo(), click());
         onView(withId(R.id.text_view_platforms_title)).perform(scrollTo());
@@ -105,7 +109,7 @@ public class GameDetailActivityTest {
     @Test
     public void testAccessToWebsite() {
 
-        onView(withId(R.id.recycler_view_games)).perform(RecyclerViewActions.scrollToPosition(0), RecyclerViewActions.actionOnItemAtPosition(0, click()));
+        goToDetail();
 
         onView(allOf(withId(R.id.text_view_website))).perform(scrollTo(), click());
     }
@@ -113,7 +117,7 @@ public class GameDetailActivityTest {
     @Test
     public void testAccessToGameSeries() {
 
-        onView(withId(R.id.recycler_view_games)).perform(RecyclerViewActions.scrollToPosition(0), RecyclerViewActions.actionOnItemAtPosition(0, click()));
+        goToDetail();
 
         onView(withId(R.id.recycler_view_game_series)).perform(scrollTo(), RecyclerViewActions.scrollToPosition(0), RecyclerViewActions.actionOnItemAtPosition(0, click()));
     }
@@ -121,7 +125,7 @@ public class GameDetailActivityTest {
     @Test
     public void testAccessToGameSuggested() {
 
-        onView(withId(R.id.recycler_view_games)).perform(RecyclerViewActions.scrollToPosition(0), RecyclerViewActions.actionOnItemAtPosition(0, click()));
+        goToDetail();
 
         onView(withId(R.id.recycler_view_games_suggested)).perform(scrollTo(), RecyclerViewActions.scrollToPosition(0), RecyclerViewActions.actionOnItemAtPosition(0, click()));
     }
@@ -129,7 +133,7 @@ public class GameDetailActivityTest {
     @Test
     public void testAccessToGameEdition() {
 
-        onView(withId(R.id.recycler_view_games)).perform(RecyclerViewActions.scrollToPosition(0), RecyclerViewActions.actionOnItemAtPosition(0, click()));
+        goToDetail();
 
         onView(withId(R.id.recycler_view_game_additions)).perform(scrollTo(), RecyclerViewActions.scrollToPosition(0), RecyclerViewActions.actionOnItemAtPosition(0, click()));
     }
@@ -175,6 +179,61 @@ public class GameDetailActivityTest {
         onView(withId(R.id.recycler_view_achievements)).check(matches(not(isDisplayed())));
     }
 
+    @Test
+    public void testLoadMoreGamesSuggested() {
+
+        goToDetail();
+
+        onView((withId(R.id.recycler_view_games_suggested))).perform(scrollTo()).check(matches(hasNumberOfChildren(GAMES_COUNT + 1)));
+        onView((withId(R.id.recycler_view_games_suggested))).perform(RecyclerViewActions.scrollToPosition(GAMES_COUNT));
+        onView(allOf(childAtPosition(withId(R.id.recycler_view_games_suggested), 1), withId(R.id.constraint_layout_load_more_items))).perform(click());
+        onView(isRoot()).perform(waitFor(TIME_TO_WAIT));
+        onView((withId(R.id.recycler_view_games_suggested))).check(matches(hasNumberOfChildren(GAMES_COUNT + GAMES_COUNT + 1)));
+        onView((withId(R.id.recycler_view_achievements))).perform(scrollTo()).check(matches(hasNumberOfChildren(GAMES_COUNT + 1)));
+    }
+
+    @Test
+    public void testLoadMoreAchievements() {
+
+        goToDetail();
+
+        onView((withId(R.id.recycler_view_achievements))).perform(scrollTo()).check(matches(hasNumberOfChildren(GAMES_COUNT + 1)));
+        onView((withId(R.id.recycler_view_achievements))).perform(RecyclerViewActions.scrollToPosition(GAMES_COUNT));
+        onView(allOf(childAtPosition(withId(R.id.recycler_view_achievements), 1), withId(R.id.constraint_layout_load_more_items))).perform(click());
+        onView(isRoot()).perform(waitFor(TIME_TO_WAIT));
+        onView((withId(R.id.recycler_view_achievements))).check(matches(hasNumberOfChildren(GAMES_COUNT + GAMES_COUNT + 1)));
+        onView((withId(R.id.recycler_view_games_suggested))).perform(scrollTo()).check(matches(hasNumberOfChildren(GAMES_COUNT + 1)));
+    }
+
+    @Test
+    public void testGoToVideo() {
+
+        goToDetail();
+
+        onView(allOf(withId(R.id.button_watch_video), isDisplayed())).perform(click());
+//        // Added a sleep statement to match the app's execution delay.
+//        // The recommended way to handle such scenarios is to use Espresso idling resources
+        onView(isRoot()).perform(waitFor(TIME_TO_WAIT));
+        onView(withId(R.id.web_view_video)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void testGoToImages() {
+
+        goToDetail();
+
+        onView(allOf(withId(R.id.button_view_images), isDisplayed())).perform(click());
+        onView(withId(R.id.grid_view_images)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void testGoBack() {
+
+        goToDetail();
+
+        onView(withContentDescription("Desplazarse hacia arriba")).perform(click());
+    }
+
     //MARK: - Private methods
 
     private void searchGame(String name) {
@@ -183,7 +242,14 @@ public class GameDetailActivityTest {
         onView(withClassName(is("android.widget.SearchView$SearchAutoComplete"))).perform(replaceText(name));
         onView(allOf(withClassName(is("android.widget.SearchView$SearchAutoComplete")), withText(name))).perform(pressImeActionButton());
         onView(isRoot()).perform(waitFor(TIME_TO_WAIT));
+
+        goToDetail();
+    }
+
+    private void goToDetail() {
+
         onView(withId(R.id.recycler_view_games)).perform(RecyclerViewActions.scrollToPosition(0), RecyclerViewActions.actionOnItemAtPosition(0, click()));
+        onView(isRoot()).perform(waitFor(TIME_TO_WAIT));
     }
 
     private static Matcher<View> hasNumberOfChildren(final int count) {
@@ -210,6 +276,22 @@ public class GameDetailActivityTest {
                 } else {
                     return ((ViewGroup) view).getChildCount() == count;
                 }
+            }
+        };
+    }
+
+    private static Matcher<View> childAtPosition(final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {}
+
+            @Override
+            public boolean matchesSafely(View view) {
+
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
             }
         };
     }
